7e251397d4e40892eadd4bfe7dd21c8d7fbcd547
Jake <jakepucci26@gmail.com>
2022-10-25 11:36:52 -0600

    added autherization/authentiaction/sessions and some more error handling - backend should be good to go

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+    skip_before_action :authorized_user
+
+
+    def create
+        user = User.find_by(name:params[:name])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user, status: :ok
+        else 
+            render json: {error: "Invalid Password or Username"}
+        end
+    end
+
+    def delete
+        session.delete(:user_id)
+        head :no_content
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   resources :events
   resources :users
 
+
+  # authentication 
+  post "/login", to: "seesions#create"
+
+  get "/authorized_user", to: "users#show"
+
+  delete "/logout", to: "sessions#delete"
+
+
   #full CRUD lists
   #full CRUD users no index
 
